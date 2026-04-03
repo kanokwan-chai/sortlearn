67299137d6958ae44d58280dc6579a1e57715b6a
kanokwan-chai <kanokwanmail2547@gmail.com>
2026-04-03 20:20:44 +0700

    Update 1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './styles/responsive.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/SortLearn">
     <App />
   </BrowserRouter>
 );
